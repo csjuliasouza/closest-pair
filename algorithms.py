273899f71322edc78ccf47d32c38075694dbedfe
juliadejaneiro <juliadejaneiro@outlook.com>
2026-03-20 19:13:40 -0400

    Initial divide-and-conquer implementation

--- algorithms.py
+++ algorithms.py
@@ -4,20 +4,21 @@ import math
 # for i in range(10000, 100001, 10000):
 #     print(i)
 
-P = [(1, 2), (2, 4), (9, 11)]
+points = [(9, 1), (3, 2), (1, 5), (2, 4)]
 
 # Brute force algorithm implementation for closest points
-def BruteForceClosestPoints(P):
+def brute_force_closest_points(p):
     d_min = math.inf
-
-    # Stop at the second to last element
-    for i in range(0, len(P)-1):
-        point_1 = P[i]
+    
+    # For each point in the array, compare the distance
+    # with successive points to find the minimum distance
+    for i in range(0, len(p)-1):
+        point_1 = p[i]
         x1, y1 = point_1[0], point_1[1]
         # print('X1:', x1)
         # print('Y1:', y1)
-        for j in range(i+1, len(P)):
-            point_2 = P[j]
+        for j in range(i+1, len(p)):
+            point_2 = p[j]
             x2, y2 = point_2[0], point_2[1]
             # print('X2:', x2)
             # print('Y2:', y2)
@@ -28,7 +29,19 @@ def BruteForceClosestPoints(P):
                 d_min, index_1, index_2 = d, i, j
     return index_1, index_2
 
-print(BruteForceClosestPoints(P))
+print(brute_force_closest_points(points))
 
 
-# Divide-and-conquer algorithm for closest points
+# Divide-and-conquer algorithm - INCOMPLETE
+def closest_pair(p):
+    p_x = sorted(p, key=lambda x: x[0])
+    p_y = sorted(p, key=lambda x: x[1])
+    # print(p_x)
+    # print(p_y)
+
+    # result = closest_pair_rec(p_x, p_y)
+
+def closest_pair_rec(p_x, p_y):
+    pass
+
+print(closest_pair(points))
